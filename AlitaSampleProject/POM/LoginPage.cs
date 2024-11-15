e7c8e92461c4851b372dfc13905f12545b4f1c6d
harshinimsri <harshini_sri@epam.com>
2024-11-15 11:36:33 +0530

    first commit

---
+++ AlitaSampleProject/POM/LoginPage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POM
+{
+    public class LoginPage
+    {
+        private By UsernameField = By.Id("user-name");
+        private By PasswordField = By.Id("password");
+        private By LoginButton = By.CssSelector("#login-button");
+    }
+}
