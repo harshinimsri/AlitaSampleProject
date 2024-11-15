d440eb97d9d9d89421a362d9fc19bf2e167267f2
harshinimsri <harshini_sri@epam.com>
2024-11-15 12:10:31 +0530

    Create Hooks/Hooks.cs

---
+++ Hooks/Hooks.cs
@@ -0,0 +1,23 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Hooks
+{
+    public class Hooks
+    {
+        public static IWebDriver driver;
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            driver.Quit();
+        }
+    }
+}
