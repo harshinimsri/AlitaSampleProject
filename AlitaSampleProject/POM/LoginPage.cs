17943cfe72f634977d790fa4ce772d57a9b2cc46
harshinimsri <harshini_sri@epam.com>
2024-11-15 13:10:08 +0530

    Update AlitaSampleProject/POM/LoginPage.cs

--- AlitaSampleProject/POM/LoginPage.cs
+++ AlitaSampleProject/POM/LoginPage.cs
@@ -7,10 +7,46 @@ using System.Threading.Tasks;
 
 namespace POM
 {
+    using OpenQA.Selenium;
+using Utilities;
+
+namespace POM
+{
     public class LoginPage
     {
+        private IWebDriver driver;
+        private WebUtility webUtility;
         private By UsernameField = By.Id("user-name");
         private By PasswordField = By.Id("password");
         private By LoginButton = By.CssSelector("#login-button");
+        private By LoginSuccessIndicator = By.CssSelector(".title");
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            webUtility = new WebUtility(driver);
+        }
+
+        public void NavigateToLoginPage()
+        {
+            webUtility.NavigateTo("https://www.saucedemo.com");
+        }
+
+        public void EnterCredentials(string username, string password)
+        {
+            webUtility.SendKeysToElement(UsernameField, username);
+            webUtility.SendKeysToElement(PasswordField, password);
+        }
+
+        public void ClickLoginButton()
+        {
+            webUtility.ClickElement(LoginButton);
+        }
+
+        public bool VerifyLoginSuccess()
+        {
+            return webUtility.WaitForElementToBeVisible(LoginSuccessIndicator).Displayed;
+        }
     }
+}
 }
