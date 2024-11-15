22b637d48f3c81feef64dddfeeb08b182192717a
harshinimsri <harshini_sri@epam.com>
2024-11-15 11:42:25 +0530

    Update AlitaSampleProject/POM/LoginPage.cs

--- AlitaSampleProject/POM/LoginPage.cs
+++ AlitaSampleProject/POM/LoginPage.cs
@@ -12,5 +12,71 @@ namespace POM
         private By UsernameField = By.Id("user-name");
         private By PasswordField = By.Id("password");
         private By LoginButton = By.CssSelector("#login-button");
+    private IWebDriver _driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void NavigateToLoginPage()
+        {
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com");
+        }
+
+        public void EnterCredentials(string username, string password)
+        {
+            _driver.FindElement(UsernameField).SendKeys(username);
+            _driver.FindElement(PasswordField).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            _driver.FindElement(LoginButton).Click();
+        }
+
+        public bool VerifyLoginSuccess()
+        {
+            return _driver.FindElement(By.ClassName("inventory_list")).Displayed;
+        }
+
+        public bool VerifyLoginFailure()
+        {
+            return _driver.FindElement(By.CssSelector("[data-test='error']")).Displayed;
+        }
+    }
+}
+private IWebDriver _driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void NavigateToLoginPage()
+        {
+            _driver.Navigate().GoToUrl("https://www.saucedemo.com");
+        }
+
+        public void EnterCredentials(string username, string password)
+        {
+            _driver.FindElement(UsernameField).SendKeys(username);
+            _driver.FindElement(PasswordField).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            _driver.FindElement(LoginButton).Click();
+        }
+
+        public bool VerifyLoginSuccess()
+        {
+            return _driver.FindElement(By.ClassName("inventory_list")).Displayed;
+        }
+
+        public bool VerifyLoginFailure()
+        {
+            return _driver.FindElement(By.CssSelector("[data-test='error']")).Displayed;
+        }
     }
 }
