0d76c76dfb22cfdb51e9ec5ee937d37253475e1c
harshinimsri <harshini_sri@epam.com>
2024-11-15 13:08:54 +0530

    Update AlitaSampleProject/Utilities/WebUtility.cs

--- AlitaSampleProject/Utilities/WebUtility.cs
+++ AlitaSampleProject/Utilities/WebUtility.cs
@@ -8,5 +8,51 @@ namespace Utilities
 {
     public class WebUtility
     {
+        private IWebDriver driver;
+
+        public void InitializeBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public IWebElement WaitForElementToBeVisible(By locator)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            return wait.Until(ExpectedConditions.ElementIsVisible(locator));
+        }
+
+        public IWebElement WaitForElementToBeClickable(By locator)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            return wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+        }
+
+        public void ClickElement(By locator)
+        {
+            WaitForElementToBeClickable(locator).Click();
+        }
+
+        public void SendKeysToElement(By locator, string text)
+        {
+            WaitForElementToBeVisible(locator).SendKeys(text);
+        }
+
+        public void NavigateTo(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void HandleAlert()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.AlertIsPresent()).Accept();
+        }
+
+        public void QuitBrowser()
+        {
+            driver.Quit();
+        }
     }
 }
