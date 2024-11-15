3c639a7e9c118e08c2aeed14057d606408115d0e
harshinimsri <harshini_sri@epam.com>
2024-11-15 12:51:43 +0530

    Update AlitaSampleProject/POM/LoginPage.cs

--- AlitaSampleProject/POM/LoginPage.cs
+++ AlitaSampleProject/POM/LoginPage.cs
@@ -1,16 +1,48 @@
 ﻿using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Utilities;
 
 namespace POM
 {
     public class LoginPage
     {
+        private IWebDriver driver;
         private By UsernameField = By.Id("user-name");
         private By PasswordField = By.Id("password");
         private By LoginButton = By.CssSelector("#login-button");
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void NavigateToLoginPage(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void EnterCredentials(string username, string password)
+        {
+            driver.FindElement(UsernameField).SendKeys(username);
+            driver.FindElement(PasswordField).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            driver.FindElement(LoginButton).Click();
+        }
+
+        public bool VerifyLoginSuccess()
+        {
+            // Assuming there's a unique element that appears only upon successful login
+            By successIndicator = By.CssSelector(".success_message");
+            try
+            {
+                return driver.FindElement(successIndicator).Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
     }
 }
